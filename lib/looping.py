7730bc4f52e2f2000239b000d4bbe372bd6f42a6
horaceo1b <horaceken0@gmail.com>
2025-08-10 20:14:14 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    """Counts down from 10 to 1 and prints 'Happy New Year!'."""
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    """Returns a list with each integer squared."""
+    return [x ** 2 for x in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    """
+    Prints numbers from 1 to 100.
+    - Multiples of 3 → 'Fizz'
+    - Multiples of 5 → 'Buzz'
+    - Multiples of 3 and 5 → 'FizzBuzz'
+    """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
